9dcb6d99b3fec224dc78b43373a77825015be31c
Nesrux <joaomarcosdevs@gmail.com>
2023-04-06 11:50:10 -0300

    Adicionando comentarios

--- Modulo - 4/MinhaPrimeiraConsulta.sql
+++ Modulo - 4/MinhaPrimeiraConsulta.sql
@@ -1,26 +1,29 @@
+-- esse código seleciona tudo da tabela de DimCustomer
 SELECT 
 	*
 FROM
 	DimCustomer
 
+--seleciona tudo da table DimStore
 SELECT
 	*
 from
 	DimStore
 
-
+--Seleciona 3 colunas da tabela DimStore
 SELECT
 	StoreKey,
 	StoreName,
 	StorePhone
 from
 	DimStore
-
+--Seleciona tudo da table Dim product
 SELECT
 	*
 from
 	DimProduct
 
+--Seleciona 2 colunas da tabela dim produto
 SELECT 
 	ProductName, 
 	BrandName 
