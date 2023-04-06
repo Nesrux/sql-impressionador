ad5ae770ffaf0d5a8361e4aee87e5122ce3b6a32
Nesrux <joaomarcosdevs@gmail.com>
2023-04-06 12:19:51 -0300

    desafio top(n) e top(n) Percent

---
+++ Modulo - 4/DesafioTop10ETopPErcent.sql
@@ -0,0 +1,35 @@
+--comando SELECT From : Retorna todas as kinhas da tabela, independente 
+	-- das colunas selecionadas
+
+SELECT
+	*
+From 
+	DimEmployee
+-- /// --- ///
+Select
+	FirstName, 
+	EmailAddress 
+from 
+	DimEmployee
+
+--COMANDOS select top(n) e TOP(n) Percent: Retorna as N primeiras linhas
+
+--1. Crie um código que retorna as 10 primeiras linhas da tablea de produtos (DimProduct)
+SELECT
+TOP(10)
+	*
+FROM
+	DimProduct
+
+--2 Retorna as 10% primeiras linhas da tabela de Clientes
+SELECT
+TOP(10)
+percent
+ *
+From 
+	DimCustomer
+
+--Comando TOP é bom para filtrar uma tabela com muitas linhas, que nesse caso abaixo
+--Tem mais de 4 milhoes de linhas
+--Tem um tempo dw execução de 6 minutos
+SELECT * FROM FactSales
