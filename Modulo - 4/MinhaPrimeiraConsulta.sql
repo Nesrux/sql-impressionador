dbb5b13a08bacc8c6f437a89dd3e33923e14f82e
Nesrux <joaomarcosdevs@gmail.com>
2023-04-06 11:44:22 -0300

    Arrumando a identação das consultas

--- Modulo - 4/MinhaPrimeiraConsulta.sql
+++ Modulo - 4/MinhaPrimeiraConsulta.sql
@@ -1,9 +1,28 @@
-SELECT * FROM DimCustomer
+SELECT 
+	*
+FROM
+	DimCustomer
 
-SELECT * from DimStore
+SELECT
+	*
+from
+	DimStore
 
-SELECT StoreKey, StoreName, StorePhone from DimStore
 
-SELECT * from DimProduct
+SELECT
+	StoreKey,
+	StoreName,
+	StorePhone
+from
+	DimStore
 
-SELECT ProductName, BrandName From DimProduct
+SELECT
+	*
+from
+	DimProduct
+
+SELECT 
+	ProductName, 
+	BrandName 
+From
+	DimProduct
