93827894aa5d23c0faa6e1942ea94a4f5db6a6f3
Nesrux <joaomarcosdevs@gmail.com>
2023-04-06 11:30:01 -0300

    Primeira consulta em SQL

---
+++ MinhaPrimeiraConsulta.sql
@@ -0,0 +1,9 @@
+SELECT * FROM DimCustomer
+
+SELECT * from DimStore
+
+SELECT StoreKey, StoreName, StorePhone from DimStore
+
+SELECT * from DimProduct
+
+SELECT ProductName, BrandName From DimProduct
