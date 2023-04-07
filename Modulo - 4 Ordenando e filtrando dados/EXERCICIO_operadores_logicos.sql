4f2df0e4120eb066c006e357872a3ea8729fb823
Nesrux <joaomarcosdevs@gmail.com>
2023-04-07 20:55:00 -0300

    Exercicio 02

--- Modulo - 4 Ordenando e filtrando dados/EXERCICIO_operadores_logicos.sql
+++ Modulo - 4 Ordenando e filtrando dados/EXERCICIO_operadores_logicos.sql
@@ -1,6 +1,18 @@
 --EXERCICIOS de fixação : AND, or e NOT
--- 1. Selecione todas as kubhas da tabela dimEmployee
+-- 1. Selecione todas as Colunas da tabela dimEmployee
 -- de funcionarios do Sexo feminino e do departamento de finanças
 
 SELECT * FROM DimEmployee
 WHERE Gender = 'F' And DepartmentName = 'Finance'
+
+--2. Selecione todas as linhas da tabela DimProduct de produtos da
+--Marca contoso e da cor vermelha e que tenham um preco maior o igual a 100
+
+SELECT
+	*
+From 
+	DimProduct
+WHERE
+	BrandName = 'Contoso'
+	And ColorName = 'Red'
+	AND UnitPrice >= 100
