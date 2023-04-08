ce6692faaf988a9657a8fff5c2f7eb009dd1e92f
Nesrux <joaomarcosdevs@gmail.com>
2023-04-08 13:47:07 -0300

    Not BetWeen

--- Modulo - 4 Ordenando e filtrando dados/BetweenEmSQL.sql
+++ Modulo - 4 Ordenando e filtrando dados/BetweenEmSQL.sql
@@ -1,4 +1,9 @@
 SELECT * From DimProduct
 WHERE UnitPrice Between 50 and 100
 
---BETWEEN Servepara procurar valores que estão entre uma coisa e outra
+--BETWEEN Servem para procurar valores que estão entre uma coisa e outra
+-- é possivel também utilizar com o operador NOT, para pesquisar tudo oque
+-- Não esta na quele range do between
+
+SELECT * FROM DimProduct
+WHERE UnitPrice NOT BetWeen  0 and 100
