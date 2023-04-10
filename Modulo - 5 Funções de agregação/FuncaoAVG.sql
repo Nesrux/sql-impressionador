490853d97ffe94df749a63db3c9d7ccaa966c596
Nesrux <joaomarcosdevs@gmail.com>
2023-04-10 15:25:23 -0300

    Função AVG

---
+++ Modulo - 5 Funções de agregação/FuncaoAVG.sql
@@ -0,0 +1,11 @@
+SELECT * FROM DimCustomer
+
+-- A função AVG ou average, é uma função que funciona 
+-- Exatamente igual as outras desse módulo
+-- A diferença é que ela faz a média total ta coluna 
+-- Exemplo:
+
+SELECT 
+	AVG(YearlyIncome) 
+From 
+	DimCustomer
