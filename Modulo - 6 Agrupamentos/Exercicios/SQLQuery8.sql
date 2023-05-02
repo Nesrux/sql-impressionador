b92a59d020f68b970a9b785ac011d85505368759
Nesrux <joaomarcosdevs@gmail.com>
2023-05-02 20:21:37 -0300

    ex 8

---
+++ Modulo - 6 Agrupamentos/Exercicios/SQLQuery8.sql
@@ -0,0 +1,16 @@
+/*
+Faça um agrupamento para descobrir a quantidade total de clientes e a média salarial de
+acordo com o seu nível escolar. Utilize a coluna Education da tabela DimCustomer para fazer
+esse agrupamento.
+*/
+
+--SELECT * FROM DimCustomer
+
+SELECT 
+	COUNT(*) AS 'Qta de clientes', 
+	AVG(YearlyIncome) AS 'média salarial',
+	Education AS 'Nivel Escolar' 
+FROM DimCustomer
+GROUP BY Education
+HAVING Education is not null
+ORDER BY AVG(YearlyIncome) 
